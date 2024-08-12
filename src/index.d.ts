44fc92361dbe4122edbd567bfeead5234fd93720
Gesang Paudra J <g.paudra@gmail.com>
2024-08-12 17:40:11 +0700

    add type

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+declare module "matauang" {
+    export function idr(jumlah: number | string): string;
+    export function usd(jumlah: number | string): string;
+    export function gbp(jumlah: number | string): string;
+    export function eur(jumlah: number | string): string;
+    export function cny(jumlah: number | string): string;
+    export function jpy(jumlah: number | string): string;
+    export function sar(jumlah: number | string): string;
+    export function rub(jumlah: number | string): string;
+    export function chf(jumlah: number | string): string;
+    export function dkk(jumlah: number | string): string;
+    export function sek(jumlah: number | string): string;
+}
